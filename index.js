1ed6b7ff3fed45bb0060475a4a5a0f1522ac141d
Michael.K <michael34435@capslock.tw>
2018-09-20 18:33:21 +0800

    feat: add routeMiddleware all

--- index.js
+++ index.js
@@ -40,56 +40,56 @@ module.exports = ({ resource, controller, routeMiddleware = {}, prefix, middlewa
   }
 
   if (resourceController.show) {
-    if (routeMiddleware.show) {
-      router.get(routes.show, routeMiddleware.show, resourceController.show)
+    if (routeMiddleware.all || routeMiddleware.show) {
+      router.get(routes.show, routeMiddleware.all || routeMiddleware.show, resourceController.show)
     } else {
       router.get(routes.show, resourceController.show)
     }
   }
 
   if (resourceController.index) {
-    if (routeMiddleware.index) {
-      router.get(routes.index, routeMiddleware.index, resourceController.index)
+    if (routeMiddleware.all || routeMiddleware.index) {
+      router.get(routes.index, routeMiddleware.all || routeMiddleware.index, resourceController.index)
     } else {
       router.get(routes.index, resourceController.index)
     }
   }
 
   if (resourceController.update) {
-    if (routeMiddleware.update) {
-      router.put(routes.update, routeMiddleware.update, resourceController.update)
+    if (routeMiddleware.all || routeMiddleware.update) {
+      router.put(routes.update, routeMiddleware.all || routeMiddleware.update, resourceController.update)
     } else {
       router.put(routes.update, resourceController.update)
     }
   }
 
   if (resourceController.edit) {
-    if (routeMiddleware.edit) {
-      router.get(routes.edit, routeMiddleware.edit, resourceController.edit)
+    if (routeMiddleware.all || routeMiddleware.edit) {
+      router.get(routes.edit, routeMiddleware.all || routeMiddleware.edit, resourceController.edit)
     } else {
       router.get(routes.edit, resourceController.edit)
     }
   }
 
   if (resourceController.create) {
-    if (routeMiddleware.create) {
-      router.get(routes.create, routeMiddleware.create, resourceController.create)
+    if (routeMiddleware.all || routeMiddleware.create) {
+      router.get(routes.create, routeMiddleware.all || routeMiddleware.create, resourceController.create)
     } else {
       router.get(routes.create, resourceController.create)
     }
   }
 
   if (resourceController.destroy) {
-    if (routeMiddleware.destroy) {
-      router.delete(routes.destroy, routeMiddleware.destroy, resourceController.destroy)
+    if (routeMiddleware.all || routeMiddleware.destroy) {
+      router.delete(routes.destroy, routeMiddleware.all || routeMiddleware.destroy, resourceController.destroy)
     } else {
       router.delete(routes.destroy, resourceController.destroy)
     }
   }
 
   if (resourceController.store) {
-    if (routeMiddleware.store) {
-      router.post(routes.store, routeMiddleware.store, resourceController.store)
+    if (routeMiddleware.all || routeMiddleware.store) {
+      router.post(routes.store, routeMiddleware.all || routeMiddleware.store, resourceController.store)
     } else {
       router.post(routes.store, resourceController.store)
     }
